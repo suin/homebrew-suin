3151c6c40d34152fb986cfc4a6174d719cc010db
suin <suinyeze@gmail.com>
2014-08-08 18:00:22 +0900

    Update iterm2-cli.rb

--- Formula/iterm2-cli.rb
+++ Formula/iterm2-cli.rb
@@ -4,6 +4,7 @@ class Iterm2Cli < Formula
   homepage "https://github.com/suin/iterm2-cli/"
   url 'https://github.com/suin/iterm2-cli/archive/1.0.0.tar.gz'
   head "https://github.com/suin/iterm2-cli.git"
+  sha1 "574d11a97487de41f8e09f0834abb7cc002fbf0d"
 
   def install
     # zsh_completion.install "etc/iterm2_completion.zsh"
