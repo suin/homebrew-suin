af9d278b66392001a0a08ef76a7a9c2d31eac71c
suin <suinyeze@gmail.com>
2014-10-14 15:03:04 +0900

    Update git-forgot.rb

--- Formula/git-forgot.rb
+++ Formula/git-forgot.rb
@@ -2,14 +2,14 @@ require "formula"
 
 class GitForgot < Formula
   homepage "https://github.com/suin/git-forgot"
-  version "71027d8"
+  version "3a00beb"
 
   if MacOS.prefer_64_bit?
     url "https://drone.io/github.com/suin/git-forgot/files/artifacts/darwin-amd64/git-forgot"
-    sha1 "17b3986ae421f482bb38d3a9a64c73a6b2db6380"
+    sha1 "d0fd03e4129224ec2dd196938f450b7f9c0889d8"
   else
     url "https://drone.io/github.com/suin/git-forgot/files/artifacts/darwin-386/git-forgot"
-    sha1 "ec748ada3361ac86e6a6f3250991333de896a052"
+    sha1 "56b04fae7c12950fb8c60a6f042392a762c58075"
   end
 
   def install
