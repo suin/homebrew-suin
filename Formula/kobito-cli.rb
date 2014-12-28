3c3edea6229b5578af57a4425314bda58e95c8ce
suin <suinyeze@gmail.com>
2014-12-28 22:05:17 +0900

    Update kobito-cli.rb

--- Formula/kobito-cli.rb
+++ Formula/kobito-cli.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class KobitoCli < Formula
   url 'https://github.com/suin/kobito-cli.git', :using => :git
-  version '2014-10-16.1'
+  version '2014-12-28'
 
   def install
     zsh_completion.install 'zsh-completions/_kobito' => '_kobito'
