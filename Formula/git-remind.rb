2f8850ee84a942a399426d20aa06f9db29c5ae07
goreleaserbot <goreleaser@carlosbecker.com>
2018-11-04 22:48:24 +0900

    Brew formula update for git-remind version v1.1.1

--- Formula/git-remind.rb
+++ Formula/git-remind.rb
@@ -1,13 +1,14 @@
 class GitRemind < Formula
   desc "Never forget to git commit and push."
   homepage "https://github.com/suin/git-remind/"
-  url "https://github.com/suin/git-remind/releases/download/v1.1.0/git-remind_1.1.0_Darwin_x86_64.tar.gz"
-  version "1.1.0"
-  sha256 "f4de205159fb3b7f64789881a2789591ef8b234979b0475fbe44d2c5dd27beed"
+  url "https://github.com/suin/git-remind/releases/download/v1.1.1/git-remind_1.1.1_Darwin_x86_64.tar.gz"
+  version "1.1.1"
+  sha256 "2ae34d9221f84fb62a3ac326b1e629bc3e56235d3bafde46af03d2eb0e6b3b25"
   
   depends_on "git"
 
   def install
+    bin.install "git-remind"
     bin.install "git-remind"
   end
 end
